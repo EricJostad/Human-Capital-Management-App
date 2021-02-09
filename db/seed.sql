a142398f5867e468b964bddf8604cd7cb66102db
EJostad <eric.jostad@outlook.com>
2021-02-08 22:50:09 -0800

    Populated seed.sql file with starter code.

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE department_DB;
+
+INSERT INTO departments (name)
+VALUES ("Engineering");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Developer", 100000.00, 1);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Eric", "Jostad", 1, 1);
